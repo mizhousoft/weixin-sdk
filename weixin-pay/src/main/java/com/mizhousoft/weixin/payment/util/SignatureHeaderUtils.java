8b28e80a858b4a973d09748a676c6b7b7a387caf
jacklee <lsp_lishuiping@qq.com>
2023-04-15 11:20:45 +0800

    解决bug

--- weixin-pay/src/main/java/com/mizhousoft/weixin/payment/util/SignatureHeaderUtils.java
+++ weixin-pay/src/main/java/com/mizhousoft/weixin/payment/util/SignatureHeaderUtils.java
@@ -1,5 +1,6 @@
 package com.mizhousoft.weixin.payment.util;
 
+import java.util.Locale;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -19,10 +20,10 @@ public abstract class SignatureHeaderUtils
 
 	public static SignatureHeader buildHeader(Map<String, String> headers)
 	{
-		String timestamp = headers.get(HttpConstants.WECHAT_PAY_TIMESTAMP);
-		String serial = headers.get(HttpConstants.WECHAT_PAY_SERIAL);
-		String nonce = headers.get(HttpConstants.WECHAT_PAY_NONCE);
-		String signature = headers.get(HttpConstants.WECHAT_PAY_SIGNATURE);
+		String timestamp = getHeaderValue(HttpConstants.WECHAT_PAY_TIMESTAMP, headers);
+		String serial = getHeaderValue(HttpConstants.WECHAT_PAY_SERIAL, headers);
+		String nonce = getHeaderValue(HttpConstants.WECHAT_PAY_NONCE, headers);
+		String signature = getHeaderValue(HttpConstants.WECHAT_PAY_SIGNATURE, headers);
 
 		SignatureHeader signHeader = new SignatureHeader();
 		signHeader.setNonce(nonce);
@@ -33,5 +34,16 @@ public abstract class SignatureHeaderUtils
 		LOG.debug("Sign Header is {}", signHeader.toString());
 
 		return signHeader;
+	}
+
+	public static String getHeaderValue(String key, Map<String, String> headers)
+	{
+		String value = headers.get(key);
+		if (null == value)
+		{
+			value = headers.get(key.toLowerCase(Locale.ENGLISH));
+		}
+
+		return value;
 	}
 }
